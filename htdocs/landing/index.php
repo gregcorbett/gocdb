c7a5baf2fed0d9e8028e9a96eaefb2cea1f40422
Greg Corbett <gregcorbett@users.noreply.github.com>
2022-05-19 09:30:25 +0000

    DO NOT MERGE: badly tweak landing page to show EOSC AAI as login option

--- htdocs/landing/index.php
+++ htdocs/landing/index.php
@@ -47,8 +47,17 @@
                 $hostname = $_SERVER['HTTP_HOST'];
                 $egi_target = urlencode("https://" . $hostname . "/portal/");
                 $egi_redirect = "https://" . $hostname . "/Shibboleth.sso/Login?target=" . $egi_target;
-              ?>
-                <a style="width:30%; display:inline-block; font-size:1.5em" href="<?php echo $egi_redirect; ?>" class="button">EGI Check-In</a>
+                if($_SERVER['REQUEST_URI'] === "/"){
+                    $iam_target = "target_link_uri=" . urlencode("https://" . $hostname . "/portal/");
+                }
+                else{
+                    $iam_target=ltrim($_SERVER['REQUEST_URI'], '/?');
+                }
+                $iris_url = urlencode("https://iris-iam.stfc.ac.uk/");
+                $iam_redirect = "https://" . $hostname . "/portal/redirect_uri?iss=" . $iris_url . "&" . $iam_target;
+                ?>
+                <a style="width:30%; display:inline-block; font-size:1.5em" href="<?php echo $egi_redirect; ?>" class="button">EOSC AAI</a>
+                <a style="width:30%; display:inline-block; font-size:1.5em"  href="<?php echo $iam_redirect ?>" class="button">IRIS IAM</a>
             </div>
             <p>Browse the <a href="https://wiki.egi.eu/wiki/GOCDB" class="docLink hover">GOCDB documentation index</a> on the EGI wiki.</p>
           </div>
@@ -75,7 +84,7 @@
             <a href="https://eoscfuture.eu/" class="Sponsor_Link" target="_blank">
               <img class="Sponsor_Logo"
                 src="/images/logos/eosc_future.png"
-                alt="The logo of the EOSC Future Horizon 20 20 project"
+                alt="The logo of the EOSC Future Horizon 20 20 project" />
             </a>
             <a href="https://www.iris.ac.uk/" class="Sponsor_Link" target="_blank">
               <img class="Sponsor_Logo"
