4a5f9c4fca7fda3a75579490e62ab91f3e48977c
gregcorbett <gregcorbett@users.noreply.github.com>
2022-06-01 07:35:59 +0000

    Increase size of logos on portal pages slightly
    - to avoid tiny-font syndrome.
    - need to tweak the UKRI logo outside the CSS because it needs to
      project above the upper line to better balance the visual
      weight of the logos.

--- htdocs/web_portal/components/Draw_Components/draw_page_components.php
+++ htdocs/web_portal/components/Draw_Components/draw_page_components.php
@@ -126,7 +126,7 @@
 
         $HTML .= '<a href="https://stfc.ukri.org/" class="Sponsor_Link" target="_blank">'.
                     /* Allow for STFC council symbol extending above the upper bound of the UKRI symbol */
-                    '<img style="height: 28px; margin: 2px; class="Sponsor_Logo" '.
+                    '<img style="height: 32px; margin: 2px; class="Sponsor_Logo" '.
                     'src="'.\GocContextPath::getPath().'img/UKRI_STF_Council-Logo_Horiz-RGB_crop.png" '.
                     'alt="The logo of the Science and Technology Facilities Council" />'.
                     '</a>';
